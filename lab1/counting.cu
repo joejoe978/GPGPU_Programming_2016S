9cbcdd947317e084972badd15be616a971a6f829
Joe Huang <r03922133@ntu.edu.tw>
2016-03-25 02:56:17 +0800

    part2 over

--- lab1/counting.cu
+++ lab1/counting.cu
@@ -6,24 +6,9 @@
 #include <thrust/functional.h>
 #include <thrust/device_ptr.h>
 #include <thrust/execution_policy.h>
-#include <thrust/remove.h>
 #include <thrust/copy.h>
 #include <thrust/reduce.h>
-
-struct not_one{
-	__host__ __device__
-	bool operator()(int x){
-		return x != 1;
-	}
-};
-	
-struct is_zero{
-    __host__ __device__
-    bool operator()(int x){
-		return x == 0;
-	}
-};
-	
+		
 struct is_one{
     __host__ __device__
 	bool operator()(int x){
@@ -98,85 +83,23 @@ void CountPosition(const char *text, int *pos, int text_size)
 
 int ExtractHead(const int *pos, int *head, int text_size)
 {
-	int *buffer, *mypos, *test;
+	int *buffer;
 	int nhead;   // number of heads
 	cudaMalloc(&buffer, sizeof(int)*text_size*2); // this is enough
 	thrust::device_ptr<const int> pos_d(pos);
 	thrust::device_ptr<int> head_d(head), flag_d(buffer), cumsum_d(buffer+text_size);
 
 	// TODO, head is answer.
-	// transform, sequence, inclusive_scan, sequence and cudaMemcpy
 	
-	thrust::minus<int> minus;
-	thrust::multiplies<int> multi;
-	thrust::divides<int> div;
 	thrust::equal_to<int> equal;
 	
-	thrust::fill(cumsum_d, cumsum_d+text_size,1);  // buffer2 = 1111111111
-	thrust::transform(pos_d,pos_d+text_size,cumsum_d,flag_d,equal);  // buffer = 01000100100
-	nhead = thrust::reduce(flag_d, flag_d+text_size); 				 // get nhead
-	printf("nhead: %d \n", nhead);
-	thrust::exclusive_scan(cumsum_d, cumsum_d+text_size, cumsum_d);  // buffer2 = 0123456789
-	thrust::transform(flag_d, flag_d+text_size, cumsum_d, cumsum_d, multi); // buffer2 = 01000500700
+	thrust::fill(cumsum_d, cumsum_d+text_size,1);  					  // buffer2 = 1111111111
+	thrust::transform(pos_d,pos_d+text_size,cumsum_d,flag_d,equal);   // buffer = 01000100100
+	nhead = thrust::reduce(flag_d, flag_d+text_size); 				  // get nhead
+	//printf("nhead: %d \n", nhead);
+	thrust::exclusive_scan(cumsum_d, cumsum_d+text_size, cumsum_d);   // buffer2 = 0123456789
 	thrust::copy_if(cumsum_d, cumsum_d+text_size, flag_d, head_d, is_one());  // get heads
-	
-	/*
-	int my[10] = {0, 1, 2, 3, 0, 1, 0, 1, 2, 3};
-	int one[10] = {1,1,1,1,1,1,1,1,1,1};
-	
-	thrust::transform(my,my+10,one,my,op);
-	for(int i=0;i<10;i++){
-		printf("%d ",my[i]);
-	}
-	printf(" \n");
-	
-	//thrust::transform_if(mypos_d, mypos_d + text_size, mypos_d, mypos_d, flag_d, minus, not_one());  // buffer = 0100010100
-	//input, input+size, input2, if data, output, op, if  
-	
-	int data[10] = {0, 1, 2, 3, 0, 1, 0, 1, 2, 3}, tmp[10] = {}, result[10] = {};
-	
-	thrust::transform_if(data, data + 10, data, data, data, minus, not_one()); 
-	// input, input+size, input2, if data, output, op, if 
-	for(int i=0;i<10;i++){
-		printf("%d ",data[i]);
-	}
-	printf(" \n");
-	
-	thrust::fill(tmp, tmp+10, 1);
-	thrust::exclusive_scan(tmp, tmp+10, tmp);
-	
-	for(int i=0;i<10;i++){
-		printf("%d ",tmp[i]);
-	}
-	printf(" \n");
-
-	//thrust::multiplies<int> multi;
-	thrust::transform(data, data+10, tmp, data, multi);  // data: 010000050008
-	for(int i=0;i<10;i++){
-		printf("%d ",data[i]);
-	}
-	printf(" \n");
-	
-	//thrust::divides<int> div;
-	thrust::fill(tmp, tmp+10, 0);
-	thrust::transform_if(data, data+10, data, data, tmp, div, not_zero()); 
-	thrust::inclusive_scan(tmp, tmp+10, tmp); 
-	
-	for(int i=0;i<10;i++){
-		printf("%d ",tmp[i]);
-	}
-	printf(" \n");
-	
-	int max = tmp[9];		
-	printf("max: %d \n",max);
-	thrust::remove_copy(data, data+10, result, 0);
-	
-	for(int i=0;i<3;i++){
-		printf("%d ",result[i]);
-	}
-	printf(" \n");
-	*/
-	
+		
 	cudaFree(buffer);
 	return nhead;  
 }
